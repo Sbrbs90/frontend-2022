52cd52f56cad649b15278af4330b9584a09f2415
Sbrbs90 <s.br.bs@hotmail.it>
2023-03-01 12:57:27 +0100

    added empy id

--- div.php
+++ div.php
@@ -1,17 +1,29 @@
+<?php
+
+    include("library.php"); //per includere la libreria di funzioni
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>esercizio</title>
 </head>
+
 <body>
     <?php
 
-        for($i = 0; $i <= 20; $i += 2){
-            echo "<div>".$i."</div>";
-        }
+    stampa_div("container", "esempio php", "head");
+
+    
+    for ($i = 0; $i <= 20; $i += 2) {
+        stampa_div("", $i); //non abbiamo dato una classe al <div> quindi mettiamo solo la variabile del contenuto
+    }
     ?>
 </body>
+
 </html>
--- library.php
+++ library.php
@@ -1,13 +1,28 @@
-//libreria di funzioni che all'occorrenza richiami
+<!--Questa è una libreria di funzioni che all'occorrenza richiami senza dover scrivere ogni volta le funzioni-->
 
 <?php
-function valore_assoluto($numero){
+
+function stampa_div($classe, $contenuto, $id = "")
+{   //funzione per stampare un <div>. Necessiti del nome della classe e il suo contenuto
+    echo "<div class='" . $classe . "'";
+    if ($id != "") {
+        echo " id = '" . $id . "'"; //se non c'è id non lo stampi
+    }
+    echo ">";
+    echo $contenuto;
+    echo "</div>";
+}
+
+
+
+function valore_assoluto($numero)
+{
     //se il valore è negativo, lo moltiplico per -1.
     //altrimenti lo restituisco senza modificarlo
 
-    if($numero < 0){
+    if ($numero < 0) {
         $risultato = $numero * -1;
-    }else{
+    } else {
         $risultato = $numero;
     }
 
