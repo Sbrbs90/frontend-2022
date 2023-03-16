566ddbc4f8a638aa9c7f2375b1daf57b00fb759f
Sbrbs90 <s.br.bs@hotmail.it>
2023-03-16 15:30:15 +0100

    add changes

--- parag.js
+++ parag.js
@@ -1,3 +1,10 @@
-let parag = document.getElementById("target");
+//esercizio1
+let parag = document.getElementById("mio-paragrafo");
+parag.textContent = "Hello, JavaScript!";
 
-parag.textContent = "questo paragrafo è scritto in Js!";
+
+//esercizio2
+function aggiungi_classe(){
+    let nuova_classe = document.getElementById("mio-div");
+    nuova_classe.classList.add("Rosso");
+}
